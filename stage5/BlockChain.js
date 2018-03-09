807ad2faa18eb45e4ef1753a6776dba7e2da0ff6
Finoramic Coder <coder@finoramic.com>
2018-03-09 15:06:25 +0530

    Create Blocks & Blockchain

---
+++ stage5/BlockChain.js
@@ -0,0 +1,31 @@
+var Block = require('./Block.js');
+var lodash = require('lodash');
+var each_cons = require('each-cons');
+
+class BlockChain {
+  constructor (msg) {
+    this.blocks = [];
+    this.generateGenesisBlock();
+  }
+
+  generateGenesisBlock() {
+    var block = new Block(null, 'Genesis Block');
+    this.blocks.push(block);
+  }
+
+  add_to_chain(msg) {
+    var last = _.last(this.blocks);
+    var block = new Block(last, msg);
+    this.blocks.push(block);
+  }
+
+  isValid () {
+    return !_.find(each_cons(this.blocks, 2), blocks =>
+      !(blocks[0].isValid() && block[1].isValid() &&
+      blocks[1].prev_block_hash === blocks[0].own_hash));
+  }
+
+  toString() {
+    _.forEach(this.blocks, block => console.log(block.toString()));
+  }
+}
